152406cf77c0e4739a8b1c02160e9215684f7622
Muhammad Daivasmara Denaw <daivasmara@gmail.com>
2018-10-02 21:21:22 +0700

    changed the main color to teal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="row">
-        <h1 class="center teal-text">Tic-Tac-Toe</h1>
-      </div>
-      <div class="gamearea row center-align teal-text">
-        <div @click="changeTurn(1)" class="col grid s2 offset-s3"><i class="material-icons">{{ area1 }}</i></div>
-        <div @click="changeTurn(2)" class="col grid s2"><i class="material-icons">{{ area2 }}</i></div>
-        <div @click="changeTurn(3)" class="col grid s2"><i class="material-icons">{{ area3 }}</i></div>
-        <div @click="changeTurn(4)" class="col grid s2 offset-s3"><i class="material-icons">{{ area4 }}</i></div>
-        <div @click="changeTurn(5)" class="col grid s2"><i class="material-icons">{{ area5 }}</i></div>
-        <div @click="changeTurn(6)" class="col grid s2"><i class="material-icons">{{ area6 }}</i></div>
-        <div @click="changeTurn(7)" class="col grid s2 offset-s3"><i class="material-icons">{{ area7 }}</i></div>
-        <div @click="changeTurn(8)" class="col grid s2"><i class="material-icons">{{ area8 }}</i></div>
-        <div @click="changeTurn(9)" class="col grid s2"><i class="material-icons">{{ area9 }}</i></div>
-      </div>
-      <div class="row">
-        <a class="waves-effect waves-light btn col s3 offset-s3" :class="{ disabled : !player1Turn }"><i class="material-icons right">panorama_fish_eye</i>PLAYER 1</a>
-        <a class="waves-effect waves-light btn col s3" :class="{ disabled : player1Turn }"><i class="material-icons right">clear</i>PLAYER 2</a>
+      <div class="tictactoe">
+        <div class="row">
+          <h1 class="center teal-text">Tic-Tac-Toe</h1>
+        </div>
+        <div class="gamearea row center-align teal-text">
+          <div @click="changeTurn(1)" class="col grid s4 m2 offset-m3"><i class="material-icons">{{ area1 }}</i></div>
+          <div @click="changeTurn(2)" class="col grid s4 m2"><i class="material-icons">{{ area2 }}</i></div>
+          <div @click="changeTurn(3)" class="col grid s4 m2"><i class="material-icons">{{ area3 }}</i></div>
+          <div @click="changeTurn(4)" class="col grid s4 m2 offset-m3"><i class="material-icons">{{ area4 }}</i></div>
+          <div @click="changeTurn(5)" class="col grid s4 m2"><i class="material-icons">{{ area5 }}</i></div>
+          <div @click="changeTurn(6)" class="col grid s4 m2"><i class="material-icons">{{ area6 }}</i></div>
+          <div @click="changeTurn(7)" class="col grid s4 m2 offset-m3"><i class="material-icons">{{ area7 }}</i></div>
+          <div @click="changeTurn(8)" class="col grid s4 m2"><i class="material-icons">{{ area8 }}</i></div>
+          <div @click="changeTurn(9)" class="col grid s4 m2"><i class="material-icons">{{ area9 }}</i></div>
+        </div>
+        <div class="row">
+          <a class="waves-effect waves-light btn col s6 m3 offset-m3" :class="{ disabled : !player1Turn }"><i class="material-icons right">panorama_fish_eye</i>PLAYER 1</a>
+          <a class="waves-effect waves-light btn col s6 m3" :class="{ disabled : player1Turn }"><i class="material-icons right">clear</i>PLAYER 2</a>
+        </div>
       </div>
     </div>
   </div>
@@ -70,13 +72,17 @@ export default {
 </script>
 
 <style lang="scss">
-.gamearea{
-  .grid{
-    height: 100px;
-    border: 1px solid #26a69a;
-    i{
-      margin-top: 35px;
+  .tictactoe{
+    margin-top: 80px;
+    .gamearea{
+      .grid{
+        height: 100px;
+        border: 1px solid #26a69a;
+        cursor: pointer;
+        i{
+          margin-top: 35px;
+        }
+      }
     }
   }
-}
 </style>
